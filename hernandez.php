09dd905c2166e9df4e12f8a20a063eef008e7af0
davy-06 <david.hernandez107@student.tsc.edu>
2025-02-06 16:27:17 -0600

    yes

---
+++ hernandez.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+    <title> This is my Homepage </title>
+</head>
+
+<body>
+
+    <center>
+        <h1> This is Mr. David's Homepage </h1>
+    </center>
+    <p> I like long runs on the evening </p>
+    <p> I also like Whiskey </p>
+    <p> I study at TSC.EDU </p>
+
+    <?php
+    echo "<br> <p> HELLO WORLD, I AM INSIDE PHP </p>";
+
+    ?>
+
+</body>
+
+</html>
